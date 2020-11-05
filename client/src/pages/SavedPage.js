ccdbe4e9fe48241304921c399e08e913177fccca
Alkawe <alixkwebb@gmail.com>
2020-11-05 11:15:35 -0500

    linked components

---
+++ client/src/pages/SavedPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../components/Header';
+import Navbar from '../components/Navbar';
+
+function SavedPage() {
+    return (
+        <div>
+            <Navbar />
+            <Header />
+            <h1>Saved Books</h1>
+        </div>
+    );
+};
+
+export default SavedPage;
--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import Search from '../components/Search';
+import Header from '../components/Header';
+import Navbar from '../components/Navbar';
+import Results from '../components/Results';
 
 function SearchPage() {
     return (
         <div>
+            <Navbar />
+            <Header />
+            <Search />
+            <Results />
         </div>
     );
 };
