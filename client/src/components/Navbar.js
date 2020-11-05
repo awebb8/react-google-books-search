b4743cc7b98b3729cf1f15e21050ead0ac2230c9
Alkawe <alixkwebb@gmail.com>
2020-11-05 10:59:54 -0500

    added Navbar component

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,9 @@
+function Navbar() {
+    return(
+        <nav class="navbar navbar-light bg-light">
+            <a class="navbar-brand">Google Books Search</a>
+        </nav>
+    )
+}
+
+export default Navbar;
